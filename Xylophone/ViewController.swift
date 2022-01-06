8b4b28a8db22a60e0ad3b79bdf391f915cc2626c
Hayley Robinson <hayley.robinson63@myhunter.cuny.edu>
2022-01-06 16:27:14 -0500

    Play different xylophone sound and delay 2 seconds to change color when button's pressed

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation // play sound
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        sender.alpha = 0.5 // dim color to half opacity of original color
+        let seconds = 0.2
+        // after 2 seconds, play sound & return to org color
+        DispatchQueue.main.asyncAfter(deadline: .now() + seconds) {
+            self.playSound(soundName: sender.currentTitle!)
+            sender.alpha = 1
+        }
+    }
     
-
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav") // gets C.wav
+        player = try! AVAudioPlayer(contentsOf: url!) // load sound
+        player.play()
+                
+    }
 }
+
 
